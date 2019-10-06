7b6463ff5fc89eea1d58aaf41aad40ddc41b0185
kdje0506 <48521269+kdje0506@users.noreply.github.com>
2019-10-06 18:03:32 +0900

    create 2557

---
+++ [2557번] Hello World.cpp
@@ -0,0 +1,22 @@
+// Hello World 성공
+// 시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+// 1 초	128 MB	203578	93363	72588	46.394%
+// 문제
+// Hello World!를 출력하시오.
+
+// 입력
+// 없음
+
+// 출력
+// Hello World!를 출력하시오.
+
+// 예제 입력 1 
+// 예제 출력 1 
+// Hello World!
+
+#include <iostream>
+using namespace std;
+int main(void) {
+	cout << "Hello World!" << endl;
+	return 0;
+}
