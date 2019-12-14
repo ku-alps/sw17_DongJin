ac87ac51ea9449a7934daaf65ecb9465b861a745
kdje0506 <48521269+kdje0506@users.noreply.github.com>
2019-12-14 13:20:23 +0900

    Create [520B]Two Buttons.cpp

---
+++ codeforces/[520B]Two Buttons.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 완전 탐색은 느리다
+void dfs(int _num, int _target, int& _min_level, int _level) {
+	if (_num <= 0 || _level >= _min_level) return;
+	if (_target == _num) {
+		if (_min_level > _level) _min_level = _level;
+		return;
+	}
+	if (_target > _num) dfs(_num * 2, _target, _min_level, _level+1);
+	dfs(_num - 1, _target, _min_level, _level+1);
+}
+// greedy로 접근
+// 목표 값이 더 작으면 무조건 -1
+// 목표 값이 더 크면 목표 값을 조정한다.
+// 이때, 짝수로 만든다음 초기값에 제일 근접하게 /2을 반복
+// 작아지면, 크기의 차이+ 나눈 반복 횟수 가 답
+int fast_version(int _n, int _target) {
+	int cnt{ 0 };
+	if (_n > _target) return _n - _target;
+
+	while (_n < _target){
+		if (_target % 2 == 0) _target /= 2;
+		else _target++;
+		cnt++;
+	}
+	return _n - _target + cnt;
+}
+int main(void) {
+	int num{}, target{};
+	cin >> num >> target;
+	cout << fast_version(num, target) << endl;
+	return 0;
+}
+
+// B. Two Buttons
+// time limit per test2 seconds
+// memory limit per test256 megabytes
+// inputstandard input
+// outputstandard output
+// Vasya has found a strange device. On the front panel of a device there are: a red button, a blue button and a display showing some positive integer. After clicking the red button, device multiplies the displayed number by two. After clicking the blue button, device subtracts one from the number on the display. If at some point the number stops being positive, the device breaks down. The display can show arbitrarily large numbers. Initially, the display shows number n.
+
+// Bob wants to get number m on the display. What minimum number of clicks he has to make in order to achieve this result?
+
+// Input
+// The first and the only line of the input contains two distinct integers n and m (1 ≤ n, m ≤ 104), separated by a space .
+
+// Output
+// Print a single number — the minimum number of times one needs to push the button required to get the number m out of number n.
+
+// Examples
+// inputCopy
+// 4 6
+// outputCopy
+// 2
+// inputCopy
+// 10 1
+// outputCopy
+// 9
+// Note
+// In the first example you need to push the blue button once, and then push the red button once.
+
+// In the second example, doubling the number is unnecessary, so we need to push the blue button nine times.
+
+
